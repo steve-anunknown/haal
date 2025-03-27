381b58757a6a6971bf0a196ac0def4dee268d9d8
steve-anunknown <steve.anunknown@gmail.com>
2025-03-27 22:12:38 +0200

    changed my mind

--- src/BlackBox.hs
+++ src/BlackBox.hs
@@ -1,12 +1,19 @@
-module Automaton
+module BlackBox
   ( Automaton (..),
+    SUL (..),
+    BlackBox (..),
   )
 where
 
 import qualified Data.Map as Map
 
-class Automaton a where
+class BlackBox a where
   step :: (Ord i, Ord s) => a i o s -> i -> (a i o s, o)
   walk :: (Ord i, Ord s) => a i o s -> [i] -> (a i o s, [o])
   current :: a i o s -> s
+
+class (BlackBox a) => Automaton a where
   transitions :: a i o s -> Map.Map (s, i) (s, o)
+
+class (BlackBox a) => SUL a where
+  reset :: (Bounded s) => a i o s -> a i o s
