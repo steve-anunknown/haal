95523db5988e564518344a78028240120d78d349
steve-anunknown <steve.anunknown@gmail.com>
2025-03-30 13:27:19 +0300

    add haddock comments and reformat

--- src/BlackBox.hs
+++ src/BlackBox.hs
@@ -1,20 +1,33 @@
-module BlackBox
-  ( Automaton (..),
+{- | This module defines the BlackBox type class as well as the Automaton and SUL
+sub classes.
+-}
+module BlackBox (
+    Automaton (..),
     SUL (..),
     BlackBox (..),
-  )
+)
 where
 
 import qualified Data.Data as Data
 import qualified Data.Map as Map
 
+{- | The 'BlackBox' type class defines the basic interface for a black box automaton.
+It provides methods to step through the automaton, walk through a list of inputs,
+and retrieve the current state.
+-}
 class BlackBox a where
-  step :: (Ord i, Ord s) => a i o s -> i -> (a i o s, o)
-  walk :: (Ord i, Ord s) => a i o s -> [i] -> (a i o s, [o])
-  current :: a i o s -> s
+    step :: (Ord i, Ord s) => a i o s -> i -> (a i o s, o)
+    walk :: (Ord i, Ord s) => a i o s -> [i] -> (a i o s, [o])
+    current :: a i o s -> s
 
+{- | The 'Automaton' type class extends the 'BlackBox' type class and adds a method
+to retrieve the transitions of the automaton.
+-}
 class (BlackBox a) => Automaton a where
-  transitions :: (Data.Data s, Data.Data i, Ord s, Ord i) => a i o s -> Map.Map (s, i) (s, o)
+    transitions :: (Data.Data s, Data.Data i, Ord s, Ord i) => a i o s -> Map.Map (s, i) (s, o)
 
+{- | The 'SUL' type class extends the 'BlackBox' type class and adds a method
+to reset the automaton to its initial state.
+-}
 class (BlackBox a) => SUL a where
-  reset :: (Bounded s) => a i o s -> a i o s
+    reset :: (Bounded s) => a i o s -> a i o s
--- src/MealyAutomaton.hs
+++ src/MealyAutomaton.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
+-- | This module implements a Mealy automaton.
 module MealyAutomaton (
     MealyAutomaton (..),
     mkMealyAutomaton,
@@ -15,6 +16,13 @@ import qualified Data.Data as Data
 import qualified Data.List as List
 import qualified Data.Map as Map
 
+{- | The 'MealyAutomaton' data type is parameterised by the 'input', 'output' and 'state' types
+ which play the role of the input alphabet, output alphabet and set of states respectively.
+ The transitions of the automaton are defined by the 'mealyDelta' and 'mealyLambda' functions,
+ which respectively return the new state after a transition and the produced output. Finally,
+ the 'mealyInitialS' defines the initial state of the automaton and the 'mealyCurrentS' defines
+ the current state which the automaton is in.
+-}
 data MealyAutomaton input output state = MealyAutomaton
     { mealyDelta :: state -> input -> state
     , mealyLambda :: state -> input -> output
@@ -22,6 +30,9 @@ data MealyAutomaton input output state = MealyAutomaton
     , mealyCurrentS :: state
     }
 
+{- | The 'mkMealyAutomaton' constructor returns a 'MealyAutomaton' by requiring the 'mealyDelta'
+function, the 'mealyLambda' function and the initial state 'mealyInitialS'.
+-}
 mkMealyAutomaton :: (s -> i -> s) -> (s -> i -> o) -> s -> MealyAutomaton i o s
 mkMealyAutomaton delta lambda initial =
     MealyAutomaton
@@ -31,6 +42,10 @@ mkMealyAutomaton delta lambda initial =
         , mealyCurrentS = initial
         }
 
+{- | The 'mkMealyAutomaton2' constructor returns a 'MealyAutomaton' by requiring just one
+function describing both the state transitions as well as the produced outputs, instead of two
+separate functions, and the initial state 'mealyInitialS'.
+-}
 mkMealyAutomaton2 :: (s -> i -> (s, o)) -> s -> MealyAutomaton i o s
 mkMealyAutomaton2 transitions initial =
     MealyAutomaton
@@ -40,6 +55,7 @@ mkMealyAutomaton2 transitions initial =
         , mealyCurrentS = initial
         }
 
+-- | Convenience function that updates the current state 'mealyCurrentS' of the automaton.
 mealyUpdateState :: MealyAutomaton i o s -> s -> MealyAutomaton i o s
 mealyUpdateState m s =
     MealyAutomaton
@@ -53,18 +69,29 @@ mealyUpdateState m s =
     lambda = mealyLambda m
     initial = mealyInitialS m
 
+{- | Performs a step in the automaton and returns a tuple containing the automaton with a modified
+state as well as the output produced by the transition.
+-}
 mealyStep :: MealyAutomaton i o s -> i -> (MealyAutomaton i o s, o)
 mealyStep m i = (mealyUpdateState m nextState, output)
   where
     nextState = mealyDelta m (mealyCurrentS m) i
     output = mealyLambda m (mealyCurrentS m) i
 
+{- | Extends the 'mealyStep' function by taking a series of inputs and performing multiple steps in
+the automaton, returning a tuple containing the automaton with a modified state as well as the
+outputs produced by the transitions.
+-}
 mealyWalk :: (Traversable t) => MealyAutomaton i o s -> t i -> (MealyAutomaton i o s, t o)
 mealyWalk = List.mapAccumL mealyStep
 
+-- | Resets the automaton to its initial state.
 mealyReset :: MealyAutomaton i o s -> MealyAutomaton i o s
 mealyReset m = mealyUpdateState m (mealyInitialS m)
 
+{- | Returns a map describing the combined behaviour of the 'mealyDelta'
+and 'mealyLambda' functions.
+-}
 mealyTransitions ::
     forall i o s.
     (Data.Data s, Data.Data i, Ord s, Ord i) =>
