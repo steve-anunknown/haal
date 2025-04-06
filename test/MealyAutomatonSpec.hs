d9bc791120e916190baf526582c7678f3b5ee56d
Stefanos Anagnostou <steve.anunknown@gmail.com>
2025-04-06 14:49:23 +0300

    more tests

--- test/MealyAutomatonSpec.hs
+++ test/MealyAutomatonSpec.hs
@@ -15,13 +15,17 @@ import qualified Data.Maybe as Maybe
 import qualified Data.Set as Set
 import MealyAutomaton (
     MealyAutomaton (..),
+    mealyAccessSequences,
     mealyDelta,
+    mealyDistinguishingSequence,
     mealyGlobalCharacterizingSet,
     mealyInAlphabet,
     mealyLambda,
     mealyOutAlphabet,
     mealyStates,
     mealyTransitions,
+    mealyUpdateState,
+    mealyWalk, mealyStep,
  )
 import Test.Hspec (Spec, context, describe, it)
 import Test.QuickCheck (Arbitrary (..), Gen, Property, choose, elements, property, vectorOf, (==>))
@@ -154,9 +158,9 @@ instance Arbitrary NonMinimalMealy where
             fallbackOutput <- arbitrary :: Gen Output
             return $ \s i -> Data.Maybe.fromMaybe fallbackOutput (Map.lookup (s, i) outputMappings)
 
--- Property to test if two states with the same delta and lambda are equivalent
-statesAreEquivalent :: NonMinimalMealy -> State -> State -> Bool
-statesAreEquivalent (NonMinimalMealy automaton) s1 s2 =
+-- Two states are equivalent if their delta and lambda functions are equivalent.
+statesAreEquivalent :: MealyAutomaton Input Output State -> State -> State -> Bool
+statesAreEquivalent automaton s1 s2 =
     all
         ( \i ->
             mealyDelta automaton s1 i == mealyDelta automaton s2 i
@@ -164,17 +168,33 @@ statesAreEquivalent (NonMinimalMealy automaton) s1 s2 =
         )
         (mealyInAlphabet automaton)
 
--- Check if the empty list [] is an element of the characterizing set
-emptyListInCharacterizingSet :: NonMinimalMealy -> State -> State -> Property
-emptyListInCharacterizingSet (NonMinimalMealy automaton) s1 s2 =
-    statesAreEquivalent (NonMinimalMealy automaton) s1 s2
+-- The global characterizing set of a non minimal mealy automaton contains
+-- the empty list.
+prop_emptyListInCharacterizingSet :: NonMinimalMealy -> State -> State -> Property
+prop_emptyListInCharacterizingSet (NonMinimalMealy automaton) s1 s2 =
+    statesAreEquivalent automaton s1 s2
         && s1
             /= s2
                 ==> []
                 `Set.member` mealyGlobalCharacterizingSet automaton
 
-mappingEquivalentToFunctions :: Mealy Input Output State -> Bool
-mappingEquivalentToFunctions (Mealy automaton) =
+-- Two states that are not equivalent can be distinguished.
+prop_existsDistinguishingSequence :: Mealy Input Output State -> State -> State -> Property
+prop_existsDistinguishingSequence (Mealy automaton) s1 s2 =
+    not (statesAreEquivalent automaton s1 s2)
+        ==> output1
+        /= output2
+        && output1 /= []
+        && output2 /= []
+  where
+    dist = mealyDistinguishingSequence automaton s1 s2
+    (_, output1) = mealyWalk (mealyUpdateState automaton s1) dist
+    (_, output2) = mealyWalk (mealyUpdateState automaton s2) dist
+
+-- The map returned by 'mealyTransitions' is equivalent to the 'mealyLambda'
+-- and 'mealyDelta' functions of the automaton.
+prop_mappingEquivalentToFunctions :: Mealy Input Output State -> Bool
+prop_mappingEquivalentToFunctions (Mealy automaton) =
     let transitions = mealyTransitions automaton
         alphabet = Set.toList $ mealyInAlphabet automaton
         states = Set.toList $ mealyStates automaton
@@ -187,15 +207,73 @@ mappingEquivalentToFunctions (Mealy automaton) =
         funOutputs = [(mealyDelta automaton s a, mealyLambda automaton s a) | s <- states, a <- alphabet]
      in mapOutputs == funOutputs
 
+-- Starts a bfs from the initial state and finds all reachable states
+findReachable :: MealyAutomaton Input Output State -> Set.Set State
+findReachable automaton =
+    let initialState = mealyInitialS automaton
+        alphabet = mealyInAlphabet automaton
+        bfs visited [] = visited
+        bfs visited (current : queue) =
+            let mo = mealyUpdateState automaton current
+                nextStates = Set.map (mealyCurrentS . fst . mealyStep mo) alphabet
+                newVisited = Set.foldr Set.insert visited nextStates
+                newQueue = queue ++ [s | s <- Set.toList nextStates, s `notElem` visited]
+             in bfs newVisited newQueue
+     in bfs (Set.singleton initialState) [initialState]
+
+prop_completeAccessSequences :: Mealy Input Output State -> Property
+prop_completeAccessSequences (Mealy automaton) = states == reachable ==> allin
+  where
+    seqs = mealyAccessSequences automaton
+    states = mealyStates automaton
+    reachable = findReachable automaton
+    allin = all (`Map.member` seqs) reachable
+
+prop_shortestAccessSequences :: Mealy Input Output State -> State -> State -> Property
+prop_shortestAccessSequences (Mealy automaton) s1 s2 =
+    s1 `Set.member` reachable
+        && s2 `Set.member` reachable
+        && existsS1toS2 ==> List.length seq2 <= List.length seq1 + 1
+  where
+    reachable = findReachable automaton
+    transitions = mealyTransitions automaton
+    accessSeqs = mealyAccessSequences automaton
+    seq1 = accessSeqs Map.! s1
+    seq2 = accessSeqs Map.! s2
+    -- find transition in map (s, i) -> (s, o)
+    -- that leads from s1 to s2
+    listed = Map.toList transitions
+    filtering (s, i) = s == s1 && fst (transitions Map.! (s, i)) == s2
+    maybeTransition = List.find filtering $ List.map fst listed
+    existsS1toS2 = case maybeTransition of
+        Nothing -> False
+        Just _ -> True
+
 spec :: Spec
 spec = do
+    describe "MealyAutomaton.mealyDistinguishingSequence" $
+        context "if 2 automatons states are not equivalent" $
+            it "returns an input sequence that distinguishes them" $
+                property
+                    prop_existsDistinguishingSequence
+
     describe "MealyAutomaton.mealyGlobalCharacterizingSet" $
         context "if the automaton contains at least 2 equivalent states" $
             it "returns a set that contains the empty list" $
                 property
-                    emptyListInCharacterizingSet
+                    prop_emptyListInCharacterizingSet
 
     describe "MealyAutomaton.mealyTransitions" $
         it "returns a map equivalent to the transition and output functions of the model" $
             property
-                mappingEquivalentToFunctions
+                prop_mappingEquivalentToFunctions
+
+    describe "MealyAutomaton.mealyAccessSequences" $
+        it "returns a map from states to list of inputs that covers all reachable states" $
+            property
+                prop_completeAccessSequences
+
+    describe "MealyAutomaton.mealyAccessSequences" $
+        it "returns a map from reachable states to shortest list of inputs that access them" $
+            property
+                prop_shortestAccessSequences
