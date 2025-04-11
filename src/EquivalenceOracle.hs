5637c64250eb74a4dc87a28b8bd3eb236341dc81
Stefanos Anagnostou <steve.anunknown@gmail.com>
2025-04-11 13:50:54 +0300

    add suite method that returns test suite

--- src/EquivalenceOracle.hs
+++ src/EquivalenceOracle.hs
@@ -7,4 +7,5 @@ import BlackBox (Automaton, SUL)
 import Data.Data (Data)
 
 class EquivalenceOracle or where
+    testSuite :: (Automaton aut, Ord i, Data i, Ord s, Data s, Eq o, Bounded s) => or -> aut i o s -> [[i]]
     findCex :: (Automaton aut, Ord i, Data i, Ord s, Data s, Eq o, Bounded s, SUL sul) => or -> aut i o s -> sul i o s -> Maybe ([i], [o])
--- src/WMethod.hs
+++ src/WMethod.hs
@@ -1,16 +1,41 @@
 module WMethod (
     WMethod (..),
     wmethod,
+    wmethodSuite,
 ) where
 
-import BlackBox (Automaton, SUL, accessSequences, globalCharacterizingSet, inputs, reset, states, walk, step)
+import BlackBox (Automaton, SUL, accessSequences, globalCharacterizingSet, inputs, reset, step, walk)
 import Control.Monad (replicateM)
 import Data.Data (Data)
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import EquivalenceOracle (EquivalenceOracle, findCex)
+import EquivalenceOracle (EquivalenceOracle, findCex, testSuite)
 
 newtype WMethod = WMethod {depth :: Int}
+
+wmethodSuite ::
+    ( Automaton aut
+    , Ord i
+    , Data i
+    , Ord s
+    , Data s
+    , Eq o
+    ) =>
+    WMethod ->
+    aut i o s ->
+    [[i]]
+wmethodSuite (WMethod{depth = d}) aut = suite
+  where
+    alphabet = Set.toList $ inputs aut
+    accessSeqs = accessSequences aut
+    characterizingSet = Set.toList $ globalCharacterizingSet aut
+    transitionCover = [a ++ [inp] | a <- Map.elems accessSeqs, inp <- alphabet]
+    suite =
+        concat
+            [ [acc ++ middle ++ char | acc <- transitionCover, char <- characterizingSet]
+            | fixed <- [0 .. d]
+            , middle <- replicateM fixed alphabet
+            ]
 
 wmethod ::
     ( Automaton aut
@@ -28,21 +53,13 @@ wmethod ::
     Maybe ([i], [o])
 wmethod (WMethod{depth = d}) aut sul = execute suite
   where
-    alphabet = Set.toList $ inputs aut
-    numStates = length $ Set.toList $ states aut
-    accessSeqs = accessSequences aut
-    characterizingSet = Set.toList $ globalCharacterizingSet aut
-    transitionCover = [a ++ [inp] | a <- Map.elems accessSeqs, inp <- alphabet]
-
-    suite = concat [ [acc ++ middle ++ char | acc <- transitionCover, char <- characterizingSet]
-                | fixed <- [0..d + numStates -1]
-                , middle <- replicateM fixed alphabet ]
+    suite = wmethodSuite (WMethod d) aut
 
     pairwiseWalk _ _ [] = True
     pairwiseWalk theSul theAut (s : ss) = (out1 == out2) && pairwiseWalk sul' aut' ss
-        where
-            (sul', out1) = step theSul s
-            (aut', out2) = step theAut s
+      where
+        (sul', out1) = step theSul s
+        (aut', out2) = step theAut s
 
     execute [] = Nothing
     execute (s : ss) = if continue then execute ss else Just (s, snd $ walk (reset sul) s)
@@ -50,4 +67,5 @@ wmethod (WMethod{depth = d}) aut sul = execute suite
         continue = pairwiseWalk (reset sul) (reset aut) s
 
 instance EquivalenceOracle WMethod where
+    testSuite = wmethodSuite
     findCex = wmethod
