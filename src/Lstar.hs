39075a4630c5afb04a6f2e287cb9c28d194e7e60
steve-anunknown <steve.anunknown@gmail.com>
2025-04-16 10:23:49 +0300

    tidied up a bit

--- src/Lstar.hs
+++ src/Lstar.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -Wno-redundant-constraints #-}
 
 module Lstar
+(
+    lstar
+)
 where
 
 import BlackBox
@@ -16,6 +20,8 @@ data ObservationTable i o = ObservationTable
     { prefixSetS :: Set.Set [i]
     , suffixSetE :: Set.Set [i]
     , mappingT :: Map.Map ([i], [i]) o
+    , -- more fields to avoid recomputing
+      prefixSetSI :: Set.Set [i]
     }
     deriving (Show)
 
@@ -39,90 +45,29 @@ equivalentRows ot r1 r2 = and $ Set.map (\e -> mapping (r1, e) == mapping (r2, e
 initializeOT ::
     forall i o s sul.
     (Bounded i, Enum i, Ord i, SUL sul, Ord s, Bounded s) =>
-    sul i o s ->
-    ObservationTable i o
-initializeOT sul =
+    Experiment (sul i o s) (ObservationTable i o)
+initializeOT = do
+    sul <- ask
     let alph = List.map (: []) $ Set.toList $ inputs sul
-        domain = (Set.singleton [] `Set.union` Set.fromList alph) `Set.cartesianProduct` Set.fromList alph
-        theMapping =
+        sm = Set.singleton []
+        sm_I = Set.fromList alph
+        em = Set.fromList alph
+        domain = (sm `Set.union` sm_I) `Set.cartesianProduct` em
+        tm =
             Map.fromList
                 [ ( (in1, in2)
                   , last $ snd $ walk (reset sul) (in1 ++ in2)
                   )
                 | (in1, in2) <- Set.toList domain
                 ]
-     in ( ObservationTable
-            { prefixSetS = Set.singleton []
-            , suffixSetE = Set.fromList alph
-            , mappingT = theMapping
+    return
+        ( ObservationTable
+            { prefixSetS = sm
+            , suffixSetE = em
+            , mappingT = tm
+            , prefixSetSI = sm_I
             }
         )
-
-otIsClosed :: forall i o. (Bounded i, Enum i, Ord i, Eq o) => ObservationTable i o -> [i]
-otIsClosed ot = Maybe.fromMaybe [] exists
-  where
-    alph = [minBound .. maxBound] :: [i]
-    sm = prefixSetS ot
-    sm_I = [w ++ [a] | w <- Set.toList sm, a <- alph]
-
-    exists = List.find (\x -> not $ any (equivalentRows ot x) sm) sm_I
-
-otIsConsistent :: forall i o. (Bounded i, Enum i, Ord i, Eq o) => ObservationTable i o -> ([i], [i])
-otIsConsistent ot = Maybe.fromMaybe ([], []) condition
-  where
-    alph = [minBound .. maxBound] :: [i]
-    sm = Set.toList $ prefixSetS ot
-
-    equivalentPairs = [(r1, r2) | r1 <- sm, r2 <- sm, r1 /= r2, equivalentRows ot r1 r2]
-
-    condition =
-        List.find
-            ( \(a, b) ->
-                any
-                    (\x -> not (equivalentRows ot (a ++ [x]) (b ++ [x])))
-                    alph
-            )
-            equivalentPairs
-
-makeClosed ::
-    forall sul i o s.
-    (Ord i, Bounded i, Enum i, SUL sul, Ord s) =>
-    ObservationTable i o ->
-    [i] ->
-    sul i o s ->
-    ObservationTable i o
-makeClosed ot [] _ = ot
-makeClosed ot inc sul = ObservationTable{prefixSetS = sm', suffixSetE = em, mappingT = tm'}
-  where
-    alph = Set.toList $ inputs sul
-    sm = prefixSetS ot
-    em = suffixSetE ot
-    tm = mappingT ot
-    sm' = inc `Set.insert` sm
-    tm' = List.foldr (uncurry Map.insert) tm [((inc ++ [s], e), last $ snd $ walk sul e) | s <- alph, e <- Set.toList em]
-
-lstar ::
-    (SUL sul, Bounded i, Enum i, Ord i, Ord s, Bounded s, Eq o) =>
-    Experiment (sul i o s) (MealyAutomaton i o StateID)
-lstar = do
-    sul <- ask
-    lstarHelp (initializeOT sul)
-
-lstarHelp ::
-    (SUL sul, Bounded i, Enum i, Ord i, Ord s, Bounded s, Eq o) =>
-    ObservationTable i o ->
-    Experiment (sul i o s) (MealyAutomaton i o StateID)
-lstarHelp ot = case otIsClosed ot of
-    [] -> case otIsConsistent ot of
-        ([], []) -> return $ makeHypothesis ot
-        inc' -> do
-            sul <- ask
-            let ot' = makeConsistent ot inc' sul
-            lstarHelp ot'
-    inc -> do
-        sul <- ask
-        let ot' = makeClosed ot inc sul
-        lstarHelp ot'
 
 -- equivalentToRow :: (Ord i, Eq o) => ObservationTable i o -> [i] -> (Set.Set [i], ObservationTable i o)
 -- equivalentToRow ot r = (equivalents, ot')
@@ -142,7 +87,7 @@ equivalenceClasses ::
 equivalenceClasses ot = go Map.empty (sm `Set.union` sm_I)
   where
     sm = prefixSetS ot
-    sm_I = Set.fromList [w ++ [a] | w <- Set.toList sm, a <- [minBound .. maxBound] :: [i]]
+    sm_I = prefixSetSI ot
     go acc s
         | Set.null s = acc
         | otherwise =
@@ -150,6 +95,52 @@ equivalenceClasses ot = go Map.empty (sm `Set.union` sm_I)
                 (equivClass, remainder) = Set.partition (equivalentRows ot x) rest
                 classMembers = x : Set.toList equivClass
              in go (Map.insert x classMembers acc) remainder
+
+lstar ::
+    (SUL sul, Bounded i, Enum i, Ord i, Ord s, Bounded s, Eq o) =>
+    Experiment (sul i o s) (MealyAutomaton i o StateID)
+lstar = do
+    ot <- initializeOT
+    lstarHelp ot
+
+lstarHelp ::
+    (SUL sul, Bounded i, Enum i, Ord i, Ord s, Bounded s, Eq o) =>
+    ObservationTable i o ->
+    Experiment (sul i o s) (MealyAutomaton i o StateID)
+lstarHelp ot = case otIsClosed ot of
+    [] -> case otIsConsistent ot of
+        ([], []) -> return $ makeHypothesis ot
+        inc' -> do
+            ot' <- makeConsistent ot inc'
+            lstarHelp ot'
+    inc -> do
+        ot' <- makeClosed ot inc
+        lstarHelp ot'
+
+otIsClosed :: forall i o. (Bounded i, Enum i, Ord i, Eq o) => ObservationTable i o -> [i]
+otIsClosed ot = Maybe.fromMaybe [] exists
+  where
+    sm = prefixSetS ot
+    sm_I = prefixSetSI ot
+
+    exists = List.find (\x -> not $ any (equivalentRows ot x) sm) sm_I
+
+otIsConsistent :: forall i o. (Bounded i, Enum i, Ord i, Eq o) => ObservationTable i o -> ([i], [i])
+otIsConsistent ot = Maybe.fromMaybe ([], []) condition
+  where
+    alph = [minBound .. maxBound] :: [i]
+    sm = Set.toList $ prefixSetS ot
+
+    equivalentPairs = [(r1, r2) | r1 <- sm, r2 <- sm, r1 /= r2, equivalentRows ot r1 r2]
+
+    condition =
+        List.find
+            ( \(a, b) ->
+                any
+                    (\x -> not (equivalentRows ot (a ++ [x]) (b ++ [x])))
+                    alph
+            )
+            equivalentPairs
 
 -- default to Int for the state type and the user can provide a mapping to whatever
 -- type they want for the state.
@@ -191,26 +182,44 @@ makeConsistent ::
     (Ord i, SUL sul, Ord s, Bounded i, Enum i) =>
     ObservationTable i o ->
     ([i], [i]) ->
-    sul i o s ->
-    ObservationTable i o
-makeConsistent ot ([], []) _ = ot
-makeConsistent ot (column, symbol) sul = ObservationTable{prefixSetS = sm, suffixSetE = em', mappingT = tm'}
-  where
-    alph = Set.toList $ inputs sul
+    Experiment (sul i o s) (ObservationTable i o)
+makeConsistent ot ([], []) = return ot
+makeConsistent ot (column, symbol) = do
+    sul <- ask
+    let
+        query = symbol ++ column
+        -- prefices = [take n query | n <- [1 .. length query]]
 
-    query = symbol ++ column
-    -- prefices = [take n query | n <- [1 .. length query]]
+        -- only the query itself must be inserted.
+        -- the suffixes are already members.
+        em = suffixSetE ot
+        em' = query `Set.insert` em
 
-    -- only the query itself must be inserted.
-    -- the suffixes are already members.
-    em = suffixSetE ot
-    em' = query `Set.insert` em
+        sm = prefixSetS ot
+        sm_I = prefixSetSI ot
 
-    sm = prefixSetS ot
-    sm_I = Set.fromList [w ++ [a] | w <- Set.toList sm, a <- alph]
+        tm = mappingT ot
 
-    tm = mappingT ot
+        missing = (sm `Set.union` sm_I) `Set.cartesianProduct` em'
 
-    missing = (sm `Set.union` sm_I) `Set.cartesianProduct` em'
+        tm' = Set.foldr (\(a, b) -> Map.insert (a, b) (last $ snd $ walk sul (a ++ b))) tm missing
+    return (ObservationTable{prefixSetS = sm, suffixSetE = em', mappingT = tm', prefixSetSI = sm_I})
 
-    tm' = Set.foldr (\(a, b) -> Map.insert (a, b) (last $ snd $ walk sul (a ++ b))) tm missing
+makeClosed ::
+    forall sul i o s.
+    (Ord i, Bounded i, Enum i, SUL sul, Ord s) =>
+    ObservationTable i o ->
+    [i] ->
+    Experiment (sul i o s) (ObservationTable i o)
+makeClosed ot [] = return ot
+makeClosed ot inc = do
+    sul <- ask
+    let
+        alph = Set.toList $ inputs sul
+        sm = prefixSetS ot
+        em = suffixSetE ot
+        tm = mappingT ot
+        sm' = inc `Set.insert` sm
+        sm_I' = Set.fromList [w ++ [a] | w <- Set.toList sm', a <- alph]
+        tm' = List.foldr (uncurry Map.insert) tm [((inc ++ [s], e), last $ snd $ walk sul e) | s <- alph, e <- Set.toList em]
+    return (ObservationTable{prefixSetS = sm', suffixSetE = em, mappingT = tm', prefixSetSI = sm_I'})
