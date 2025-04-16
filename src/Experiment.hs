9ea3083276ddd2e767c8d0128261490c6629eb78
steve-anunknown <steve.anunknown@gmail.com>
2025-04-16 15:11:09 +0300

    move equivalence oracle in experiment

--- src/Experiment.hs
+++ src/Experiment.hs
@@ -1,23 +1,111 @@
 module Experiment (
-    experiment,
     Experiment,
+    Learner (..),
+    EquivalenceOracle (..),
 ) where
 
 import Control.Monad.Reader
 
+import BlackBox (Automaton, SUL)
+
+class EquivalenceOracle or where
+    testSuiteSize ::
+        ( Automaton aut
+        , Ord i
+        , Bounded i
+        , Enum i
+        , Ord s
+        , Eq o
+        , Bounded s
+        , Enum s
+        ) =>
+        or ->
+        aut i o s ->
+        Int
+    testSuite ::
+        ( Automaton aut
+        , Ord i
+        , Bounded i
+        , Enum i
+        , Ord s
+        , Bounded s
+        , Enum s
+        , Eq o
+        ) =>
+        or ->
+        aut i o s ->
+        [[i]]
+    findCex ::
+        ( Automaton aut
+        , Ord i
+        , Bounded i
+        , Enum i
+        , Ord s
+        , Bounded s
+        , Enum s
+        , Eq o
+        , SUL sul
+        ) =>
+        or ->
+        aut i o s ->
+        Experiment (sul i o s) (Maybe ([i], [o]))
+
+class Learner l where
+    initialize ::
+        ( SUL sul
+        , Bounded i
+        , Enum i
+        , Ord i
+        , Bounded o
+        , Enum o
+        , Bounded s
+        , Enum s
+        , Ord s
+        ) =>
+        l i o ->
+        Experiment (sul i o s) (l i o)
+    refine ::
+        ( SUL sul
+        , Bounded i
+        , Enum i
+        , Ord i
+        , Bounded o
+        , Enum o
+        , Bounded s
+        , Enum s
+        , Ord s
+        ) =>
+        l i o ->
+        [i] ->
+        Experiment (sul i o s) (l i o)
+    learn ::
+        ( SUL sul
+        , Automaton aut
+        , Bounded i
+        , Enum i
+        , Ord i
+        , Bounded o
+        , Enum o
+        , Eq o
+        , Bounded s
+        , Enum s
+        , Ord s
+        ) =>
+        l i o ->
+        Experiment (sul i o s) (l i o, aut i o s)
+
 type Experiment sul result = Reader sul result
 
-experiment :: p1 -> (p1 -> t1 -> Maybe t2) -> (p1 -> p2 -> t1) -> t1
-experiment sul teacher learner = go emptyState
-  where
-    emptyState = undefined
-    updateState = undefined
-
-    go state =
-        let
-            h = learner sul state
-            cex = teacher sul h
-         in
-            case cex of
-                Nothing -> h
-                Just ce -> go (updateState state ce)
+--
+-- experiment ::
+--     (SUL sul) =>
+--     Learner sul i o sid -> -- learner
+--     Teacher sul i o sid -> -- teacher
+--     Refiner sul i o sid -> -- refiner
+--     Experiment (sul i o sid) (MealyAutomaton i o sid)
+-- experiment learner teacher refine = do
+--     h <- learner
+--     cex <- teacher h
+--     case cex of
+--         Nothing -> return h
+--         Just ce -> refine h ce >>= \h' -> experiment (return h') teacher refine
