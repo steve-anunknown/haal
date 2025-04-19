118b45e91f8725733184877590e5dcfde7b95cce
Stefanos Anagnostou <steve.anunknown@gmail.com>
2025-04-19 20:03:36 +0300

    update oracle instance

--- src/WMethod.hs
+++ src/WMethod.hs
@@ -1,17 +1,14 @@
 -- | This module implements the W-method equivalence oracle.
 module WMethod (
     WMethod (..),
-    wmethod,
-    wmethodSuite,
     wmethodSuiteSize,
 ) where
 
-import BlackBox (Automaton, SUL, accessSequences, globalCharacterizingSet, inputs, reset, step, walk)
+import BlackBox (Automaton, accessSequences, globalCharacterizingSet, inputs)
 import Control.Monad (replicateM)
-import Control.Monad.Reader
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import Experiment (EquivalenceOracle (..), Experiment)
+import Experiment (EquivalenceOracle (..))
 
 {- | The 'WMethod' type represents the W-method equivalence oracle.
 It is just a wrapper around an integer, which is used for configuring
@@ -68,39 +65,5 @@ wmethodSuite (WMethod{depth = d}) aut = suite
             , middle <- replicateM fixed alphabet
             ]
 
--- | The 'wmethod' function implements the W-method equivalence oracle.
-wmethod ::
-    ( Automaton aut s
-    , Ord i
-    , Ord s
-    , Eq o
-    , Bounded s
-    , SUL sul
-    , Bounded i
-    , Enum i
-    , Enum s
-    ) =>
-    WMethod ->
-    aut i o ->
-    Experiment (sul i o) (Maybe ([i], [o]))
-wmethod (WMethod{depth = d}) aut = do
-    sul <- ask
-    let
-        suite = wmethodSuite (WMethod d) aut
-
-        pairwiseWalk _ _ [] = True
-        pairwiseWalk theSul theAut (s : ss) = (out1 == out2) && pairwiseWalk sul' aut' ss
-          where
-            (sul', out1) = step theSul s
-            (aut', out2) = step theAut s
-
-        execute [] = Nothing
-        execute (s : ss) = if continue then execute ss else Just (s, snd $ walk (reset sul) s)
-          where
-            continue = pairwiseWalk (reset sul) (reset aut) s
-    return $ execute suite
-
 instance EquivalenceOracle WMethod where
-    testSuiteSize = wmethodSuiteSize
     testSuite = wmethodSuite
-    findCex = wmethod
