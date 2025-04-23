4ea2afd544d8c04c7cf9fc7ebc894370f96fd39a
steve-anunknown <steve.anunknown@gmail.com>
2025-04-23 19:31:14 +0300

    implemented random w method

--- src/EquivalenceOracle/WMethod.hs
+++ src/EquivalenceOracle/WMethod.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 -- | This module implements the W-method equivalence oracle.
 module EquivalenceOracle.WMethod (
     WMethod (..),
@@ -6,9 +8,12 @@ module EquivalenceOracle.WMethod (
 
 import BlackBox (Automaton, accessSequences, globalCharacterizingSet, inputs)
 import Control.Monad (replicateM)
+import qualified Data.List as List
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import Experiment (EquivalenceOracle (..))
+import EquivalenceOracle.RandomWords
+import Experiment
+import System.Random (Random (randomR), RandomGen (split), StdGen)
 
 {- | The 'WMethod' type represents the W-method equivalence oracle.
 It is just a wrapper around an integer, which is used for configuring
@@ -38,7 +43,7 @@ wmethodSuiteSize (WMethod{depth = d}) aut = size
     transitionCover = accessSeqs * alphabet
     size = sum [transitionCover * (alphabet ^ n) * characterizingSet | n <- [0 .. d]]
 
--- | The 'wmethodSuite' function generates the test suite for the W-method.
+-- | The 'wmethodSuite' function generates the test suite for the W-method and a new oracle.
 wmethodSuite ::
     ( Automaton aut s
     , Ord i
@@ -52,7 +57,7 @@ wmethodSuite ::
     WMethod ->
     aut i o ->
     (WMethod, [[i]])
-wmethodSuite (WMethod{depth = d}) aut = (WMethod {depth = d}, suite)
+wmethodSuite (WMethod{depth = d}) aut = (WMethod{depth = d}, suite)
   where
     alphabet = Set.toList $ inputs aut
     accessSeqs = accessSequences aut
@@ -67,3 +72,53 @@ wmethodSuite (WMethod{depth = d}) aut = (WMethod {depth = d}, suite)
 
 instance EquivalenceOracle WMethod where
     testSuite = wmethodSuite
+
+-- | The 'RandomWMethodConfig' type is used to configure the random W-method.
+data RandomWMethodConfig
+    = RandomWMethodConfig
+        -- | Random generator
+        StdGen
+        -- | Walks per state
+        Int
+        -- | Length of walk
+        Int
+    deriving (Show, Eq)
+
+-- | The 'RandomWMethod' type represents a random W-method equivalence oracle.
+newtype RandomWMethod = RandomWMethod RandomWMethodConfig deriving (Show, Eq)
+
+-- | The 'randomWMethodSuite' function generates the test suite for the random W-method and a new oracle.
+randomWMethodSuite ::
+    forall i o s aut.
+    ( Automaton aut s
+    , Ord i
+    , Ord s
+    , Eq o
+    , Bounded i
+    , Bounded s
+    , Enum i
+    , Enum s
+    ) =>
+    RandomWMethod ->
+    aut i o ->
+    (RandomWMethod, [[i]])
+randomWMethodSuite (RandomWMethod (RandomWMethodConfig g wpr wl)) aut = (RandomWMethod (RandomWMethodConfig gen''' wpr wl), suite)
+  where
+    shorthand = error "RandomWMethodSuite: This shouldn't be evaluated" :: aut i o
+    rorc = RandomWords g wpr 0 wl
+    prefixes = accessSequences aut
+    globalCharSet = globalCharacterizingSet aut
+
+    (RandomWords gen' _ _ _, randomwords) = List.mapAccumL testSuite rorc (replicate (length prefixes) shorthand)
+    randomWords = concat randomwords
+
+    (gen'', gen''') = split gen'
+
+    -- make a random choice from globalCharSet (length prefix) times (wpr)
+    randomSuffixes = take (length prefixes * wpr) $ List.unfoldr (Just . randomR (0, length globalCharSet - 1)) gen''
+    suffixes = map (Set.toList globalCharSet !!) randomSuffixes
+
+    suite = [prefix ++ rand ++ suffix | prefix <- Map.elems prefixes, rand <- randomWords, suffix <- suffixes]
+
+instance EquivalenceOracle RandomWMethod where
+    testSuite = randomWMethodSuite
