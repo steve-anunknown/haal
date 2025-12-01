323a6af388deabf524f485cc279eedc495015c89
Stefanos Anagnostou <steve.anunknown@gmail.com>
2025-12-01 18:00:19 +0200

    i can learn my website in just a gazillion (7) minutes

--- examples/website.hs
+++ examples/website.hs
@@ -1,19 +1,44 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TupleSections #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
 
-import Control.Monad.IO.Class (MonadIO (liftIO))
 import qualified Data.List as List
 import Haal.BlackBox
+import Haal.EquivalenceOracle.WMethod (mkWMethod)
+import Haal.Experiment
+import Haal.Learning.LMstar
 import System.Process (readProcess)
-import Control.Monad ((<=<))
 
-data Page = Home | About | CV | Links | Blogs
-    deriving (Eq, Show)
-data PageTag = LandingTag | AboutTag | CVTag | LinksTag | BlogsTag | NotFoundTag
+--------------------------------------------------------------------------------
+-- Types
+--------------------------------------------------------------------------------
+
+data Page = Home | About | CV | Links | Blogs | WrongUrl
+    deriving (Eq, Show, Ord, Enum, Bounded)
+
+data PageTag
+    = LandingTag
+    | AboutTag
+    | CVTag
+    | LinksTag
+    | BlogsTag
+    | NotFoundTag
+    deriving (Eq, Show, Ord, Enum, Bounded)
+
+data WebsiteSUL inp out = WebsiteSUL
+    { baseUrl :: String
+    , currentUrl :: String -- current path, e.g. "/about"
+    }
     deriving (Eq, Show)
 
-baseUrl :: String
-baseUrl = "http://www.anunknown.me"
+-- Fetch + HTML processing
+--------------------------------------------------------------------------------
+
+fetch :: String -> IO String
+fetch url = readProcess "curl" ["-sL", url] ""
+
+--------------------------------------------------------------------------------
+-- Input and output abstraction
+--------------------------------------------------------------------------------
 
 inputMap :: Page -> String
 inputMap Home = "/"
@@ -21,9 +46,7 @@ inputMap About = "/about"
 inputMap CV = "/education-and-work"
 inputMap Links = "/links"
 inputMap Blogs = "/blogs"
-
-fetchPage :: String -> IO String
-fetchPage url = readProcess "curl" ["-sL", url] ""
+inputMap WrongUrl = "/garbage"
 
 outputMap :: String -> PageTag
 outputMap html
@@ -34,19 +57,53 @@ outputMap html
     | "<title>Stefanos" `List.isInfixOf` html = LandingTag
     | otherwise = NotFoundTag
 
--- sul type must be parameterized by input output, which
--- I now see that is a bit silly.
-newtype WebsiteSUL input output = WebsiteSUL String
+-- >>> html <- fetch "http://www.anunknown.me/blogs"
+-- >>> outputMap html
+-- BlogsTag
+
+--------------------------------------------------------------------------------
+-- SUL instance
+--------------------------------------------------------------------------------
 
 instance SUL WebsiteSUL IO Page PageTag where
-    step system input = do
-        let url = inputMap input
-        html <- fetchPage url
-        let tag = outputMap html
-        return (system, tag)
-    reset = return
+    reset sul = do
+        pure sul{currentUrl = "/"}
 
-example system input = (<=<)
+    step sul input = do
+        if currentUrl sul /= "/garbage"
+            then do
+                let suffix = inputMap input
+                html <- fetch (baseUrl sul ++ suffix)
+                let out = outputMap html
+                    sul' = sul{currentUrl = suffix}
+                pure (sul', out)
+            else do
+                pure (sul, NotFoundTag)
+
+--------------------------------------------------------------------------------
+-- Experiment setup
+--------------------------------------------------------------------------------
+
+learner = mkLMstar Star
+teacher = mkWMethod 2
+exper = experiment learner teacher
+
+--------------------------------------------------------------------------------
+-- Main
+--------------------------------------------------------------------------------
 
 main :: IO ()
-main = undefined
+main = do
+    putStrLn "Learning Experiment"
+    putStrLn "==================="
+    putStrLn "System Under Learning: my personal website"
+    let _baseUrl = "http://www.anunknown.me"
+        _currentUrl = "/"
+    let website =
+            WebsiteSUL
+                { baseUrl = _baseUrl -- this is constant
+                , currentUrl = _currentUrl
+                } ::
+                WebsiteSUL Page PageTag
+    (model, _) <- runExperimentT exper website
+    putStrLn $ "Learned Model: " ++ show model
