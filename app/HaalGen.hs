4e6e368cc63949b02e59286f32cd76284b615788
Stefanos Anagnostou <steve.anunknown@gmail.com>
2026-03-21 17:48:50 +0200

    add help message

--- app/HaalGen.hs
+++ app/HaalGen.hs
@@ -7,18 +7,41 @@
 module Main (main) where
 
 import System.Environment (getArgs)
-import System.Exit (exitFailure)
+import System.Exit (exitFailure, exitSuccess)
 import System.IO (hPutStrLn, stderr)
 
 import Haal.Dot (parseDot, generateModule, parsedWarnings)
+
+helpText :: String
+helpText = unlines
+    [ "haal-gen - generate a Haskell module from a Mealy automaton in DOT format"
+    , ""
+    , "Usage:"
+    , "  haal-gen <dotfile> <ModuleName> <valueName>"
+    , ""
+    , "Arguments:"
+    , "  <dotfile>     Path to a DOT file (AALpy or LearnLib format)"
+    , "  <ModuleName>  Fully qualified Haskell module name (e.g. My.Module.Name)"
+    , "  <valueName>   Name for the generated automaton value (e.g. myAutomaton)"
+    , ""
+    , "Output:"
+    , "  Haskell source is written to stdout; warnings and errors to stderr."
+    , "  The generated module contains typed input/output ADTs and a"
+    , "  MealyAutomaton value, ready to import and use with haal."
+    , ""
+    , "Example:"
+    , "  haal-gen model.dot Haal.Models.Foo fooModel > src/Haal/Models/Foo.hs"
+    ]
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
+        ["--help"] -> putStr helpText >> exitSuccess
         [dotFile, modName, valName] -> run dotFile modName valName
         _ -> do
             hPutStrLn stderr "Usage: haal-gen <dotfile> <ModuleName> <valueName>"
+            hPutStrLn stderr "Run 'haal-gen --help' for more information."
             exitFailure
 
 run :: FilePath -> String -> String -> IO ()
